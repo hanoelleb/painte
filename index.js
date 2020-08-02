b5d9f376dfa3bb813c189979a22975a1d47acc36
hanoelleb <hanoelleb@gmail.com>
2020-08-02 15:39:41 -0700

    can draw on canvas

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
 $(document).ready(function() {
+   var isDrawing = false;
+
    $('#canvas').on('mouseover', function() {
        console.log('in canvas');
+   });
+
+   $('#canvas').on('mousedown', function(e) {
+       isDrawing = true;
+
+       var ctx = $(this)[0].getContext('2d');
+
+       var x = e.clientX;
+       var y = e.clientY;
+   
+       ctx.beginPath();
+       ctx.moveTo(x,y);
+
+       console.log('holding');
+   });
+
+   $('#canvas').on('mousemove', function(e) {
+       if (isDrawing) {
+           var ctx = $(this)[0].getContext('2d');
+
+           var x = e.clientX;
+           var y = e.clientY;
+
+           ctx.lineTo(x,y);
+           ctx.stroke();
+       }
+   });
+
+   $('#canvas').on('mouseup', function() {
+       isDrawing = false;
    });
 })
